2ab297705b25a475416ca19c019390dbd7a0ceaa
JonathanRys <jonathan.rk.rys@gmail.com>
2025-07-21 17:32:20 -0400

    start playing with maps and geometries

--- weather.py
+++ weather.py
@@ -1,7 +1,13 @@
 import requests
 import pandas as pd
+from shapely import wkt
+import geopandas as gpd
+import contextily as ctx
 import matplotlib.pyplot as plt
-from config import WEATHER_GOV_API, WEATHER_GOV_HEADERS, WEATHER_GOV_STATIONS_API, WEATHER_GOV_FORECAST_API, WEATHER_GOV_FORECAST_HOURLY_API, WEATHER_GOV_RADAR_STATIONS_API, WEATHER_GOV_OBSERVATIONS_API
+from config import WEATHER_GOV_API, WEATHER_GOV_HEADERS, WEATHER_GOV_STATIONS_API, WEATHER_GOV_ZONES_API, WEATHER_GOV_FORECAST_API, WEATHER_GOV_FORECAST_HOURLY_API, WEATHER_GOV_RADAR_STATIONS_API, WEATHER_GOV_OBSERVATIONS_API
+
+
+client_timezone = 'America/New_York'
 
 def get_radar_stations():
     """
@@ -47,23 +53,19 @@ def get_radar_stations():
         print(f"Error fetching radar stations: {response.status_code} - {response.text}")
         return []
 
+def get_zones():
+    """
+    Fetches zones from the National Weather Service API.
+    Returns a list of zones with their details.
+    """
+    response = requests.get(WEATHER_GOV_ZONES_API, headers=WEATHER_GOV_HEADERS)
+    if response.status_code == 200:
+        return response.json().get('@graph', [])
+    else:
+        print(f"Error fetching zones: {response.status_code} - {response.text}")
+        return []
 
 def get_stations(state='MA'):
-    """
-    Fetches weather stations from the National Weather Service API.
-    Returns: {
-        '@id': 'https://api.weather.gov/stations/022PG',
-        '@type': 'wx:ObservationStation',
-        'geometry': 'POINT(-120.11951 38.13141)',
-        'elevation': {'unitCode': 'wmoUnit:m', 'value': 1615.44},
-        'stationIdentifier': '022PG',
-        'name': 'Long Barn',
-        'timeZone': 'America/Los_Angeles', 
-        'forecast': 'https://api.weather.gov/zones/forecast/CAZ069',
-        'county': 'https://api.weather.gov/zones/county/CAC109',\
-        'fireWeatherZone': 'https://api.weather.gov/zones/fire/CAZ221'
-    }
-    """
     stations = []
     url = f"{WEATHER_GOV_STATIONS_API}?limit=500&state={state}"
 
@@ -84,51 +86,7 @@ def get_stations(state='MA'):
     # Filter by timezone after collecting all stations
     return stations
 
-def get_station(station_id):
-    """
-    Fetches detailed information about a specific weather station.
-    """
-    station_url = f"{WEATHER_GOV_STATIONS_API}/{station_id}"
-    response = requests.get(station_url, headers=WEATHER_GOV_HEADERS)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error fetching station {station_id}: {response.status_code} - {response.text}")
-        return None
-
 def get_observations(station_id):
-    """
-    Fetches observations for a specific weather station.
-    Returns: {
-        '@id': 'https://api.weather.gov/stations/0258W/observations/2025-06-18T13:10:00+00:00', 
-        '@type': 'wx:ObservationStation', 
-        'geometry': 'POINT(-71.17 42.33)', 
-        'elevation': {'unitCode': 'wmoUnit:m', 'value': 68}, 
-        'station': 'https://api.weather.gov/stations/0258W', 
-        'stationId': '0258W', 
-        'stationName': 'Boston College', 
-        'timestamp': '2025-06-18T13:10:00+00:00', 
-        'rawMessage': '', 
-        'textDescription': '', 
-        'icon': None, 
-        'presentWeather': [], 
-        'temperature': {'unitCode': 'wmoUnit:degC', 'value': 19.67, 'qualityControl': 'V'}, 
-        'dewpoint': {'unitCode': 'wmoUnit:degC', 'value': None, 'qualityControl': 'Z'}, 
-        'windDirection': {'unitCode': 'wmoUnit:degree_(angle)', 'value': 194, 'qualityControl': 'V'}, 
-        'windSpeed': {'unitCode': 'wmoUnit:km_h-1', 'value': 8.028, 'qualityControl': 'V'}, 
-        'windGust': {'unitCode': 'wmoUnit:km_h-1', 'value': None, 'qualityControl': 'Z'}, 
-        'barometricPressure': {'unitCode': 'wmoUnit:Pa', 'value': None, 'qualityControl': 'Z'}, 
-        'seaLevelPressure': {'unitCode': 'wmoUnit:Pa', 'value': None, 'qualityControl': 'Z'}, 
-        'visibility': {'unitCode': 'wmoUnit:m', 'value': None, 'qualityControl': 'Z'}, 
-        'maxTemperatureLast24Hours': {'unitCode': 'wmoUnit:degC', 'value': None}, 
-        'minTemperatureLast24Hours': {'unitCode': 'wmoUnit:degC', 'value': None}, 
-        'precipitationLast3Hours': {'unitCode': 'wmoUnit:mm', 'value': None, 'qualityControl': 'Z'},
-        'relativeHumidity': {'unitCode': 'wmoUnit:percent', 'value': None, 'qualityControl': 'V'}, 
-        'windChill': {'unitCode': 'wmoUnit:degC', 'value': None, 'qualityControl': 'V'}, 
-        'heatIndex': {'unitCode': 'wmoUnit:degC', 'value': None, 'qualityControl': 'V'}, 
-        'cloudLayers': []
-    }
-    """
     url = WEATHER_GOV_OBSERVATIONS_API.format(station_id=station_id)
     response = requests.get(url, headers=WEATHER_GOV_HEADERS)
     if response.status_code == 200:
@@ -136,52 +94,78 @@ def get_observations(station_id):
     else:
         print(f"Error fetching observations for {station_id}: {response.status_code} - {response.text}")
         return []
-
-def get_forecast(station_id, x=0, y=0):
-    url = WEATHER_GOV_FORECAST_API.format(station_id=station_id, x=x, y=y)
+    
+def fetch(url):
     response = requests.get(url, headers=WEATHER_GOV_HEADERS)
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Error fetching forecast for {station_id}: {response.status_code} - {response.text}")
+        print(f"Error fetching data from {url}: {response.status_code} - {response.text}")
         return None
-    
 
 if __name__ == "__main__":
+    print('Fetching observation stations...')
     stations = get_stations()
-    # filtered_data = [{'id': station['stationIdentifier'], 'name': station['name']} for station in stations if not station['name'].startswith(station['stationIdentifier'])]
-    # filtered_data = [{'id': station['stationIdentifier'], 'name': station['name']} for station in stations]
-    # print(len([filtered_data]), [station['name'] for station in filtered_data])
+    if not stations:
+        print('No stations found.')
+        exit()
 
-    radar_stations = get_radar_stations()
-    # print(len(radar_stations), radar_stations)
+    print(f'Found {len(stations)} stations.')
 
-    # stations = get_stations()
-    # simple_stations = [{'id': station['stationIdentifier'], 'name': station['name']} for station in stations]
-    # print(len([stations]), [station['name'] for station in stations])
+    # 260
+    selected_station_index = 260
 
-    # print('station data:', get_station(simple_stations[0]['id']))
+    station_name = stations[selected_station_index]['name']
+    station_county_url = stations[selected_station_index]['county']
+    station_id = stations[selected_station_index]['stationIdentifier']
 
-    station_name = stations[0]['name']
-    station_id = stations[0]['stationIdentifier']
+    zone = fetch(station_county_url)
+    # print(zone)
+    print(f"zone info: ({zone['type']}) name: {zone['name']}, {zone['state']} - wfo: {zone['gridIdentifier']}")
+
+    zone_wfo = zone['gridIdentifier']
+    zone_geometry = wkt.loads(zone['geometry'])
+    x, y = zone_geometry.exterior.xy
+    
+    # Create a GeoDataFrame from your polygon
+    gdf = gpd.GeoDataFrame({'geometry': [zone_geometry]}, crs='EPSG:4326')  # WGS84
+
+    # Convert to Web Mercator for contextily
+    gdf = gdf.to_crs(epsg=3857)
+
+    # Plot
+    ax = gdf.plot(figsize=(10, 10), alpha=0.5, edgecolor='k')
+    print(ctx.providers)
+    ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title('Polygon on World Map')
+    plt.show()
+
+    exit(1)
+
+    # print('forecast URL:', WEATHER_GOV_FORECAST_API.format(wfo=zone_wfo, x=zone_x, y=zone_y))
+
+    print(f'\nFetching observations for station "{station_name}" ({station_id})...')
 
     observations = get_observations(station_id)
     observations.pop() # remove last data point because the timestamp is very different from the rest
     temp_wind_data = pd.DataFrame([{
         'temperature': observation['temperature'].get('value'),
         'windSpeed': observation['windSpeed'].get('value'),
-        'timestamp': pd.to_datetime(observation['timestamp']
+        'timestamp': pd.to_datetime(observation['timestamp'], utc=True).tz_convert(client_timezone
     )} for observation in observations])
 
+    print(f'observations for station {station_name}:\n', temp_wind_data)
+
     temp_wind_data = temp_wind_data.set_index('timestamp')
-    resampled_data = temp_wind_data.resample('10T').mean().interpolate(method='linear')
+    resampled_data = temp_wind_data.resample('10min').mean().interpolate(method='linear')
 
-
-    print('observations for station {station_name}:\n', temp_wind_data)
+    # print(f'observations for station {station_name}:\n', temp_wind_data)
 
     plt.plot(resampled_data.index, resampled_data['temperature'], label='Temperature (°C)')
     plt.plot(resampled_data.index, resampled_data['windSpeed'], label='Wind Speed (km/h)')
-    plt.xlabel('Timestamp')
+    plt.xlabel('Date')
     plt.ylabel('Value')
     plt.legend()
     plt.tight_layout()
